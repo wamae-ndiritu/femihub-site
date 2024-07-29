c66c303afb22cece141ca2aa697f55aa0cb2bda3
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-07-29 14:16:23 +0300

    Update maternal and HIV content

---
+++ client/src/components/MaternalWellness.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import maternalWellness from "../assets/maternal-1.png";
+
+const MaternalWellness = () => {
+  return (
+    <section className='bg-white pb-16 px-4 md:px-16'>
+      <div className='md:max-w-7xl mx-auto flex flex-col items-center'>
+        <h2 className='text-2xl md:text-4xl font-bold text-custom-pink my-8 text-center'>
+          Maternal Wellness
+        </h2>
+        <div className='w-full flex justify-center mb-8'>
+          <img
+            src={maternalWellness}
+            alt='Maternal Wellness Information'
+            className='w-full md:w-4/5 md:h-[400px]'
+          />
+        </div>
+        <div className='w-full'>
+          <h2 className='text-gray-900 font-semibold mb-4 text-3xl'>
+            Our Mother and Child Health services include:
+          </h2>
+          <ul className='list-disc px-6 md:px-16'>
+            <li>
+              <h3 className='text-2xl font-semibold mb-2'>Antenatal Care</h3>
+              <p className='text-gray-700 mb-4'>
+                Through antenatal care (ANC) we will help both you and your
+                unborn baby stay healthy. Even if your pregnancy is going well
+                and you’re feeling well, it’s important for you to attend your
+                ANC appointments so that any potential risks can be identified
+                and prevented, or reduced. During ANC you will have an
+                opportunity to ask any questions you have about your pregnancy
+                such as what's happening during each trimester, physical
+                pregnancy symptoms and the birth itself and also about caring
+                for your baby after the birth.
+              </p>
+            </li>
+            <li>
+              <h3 className='text-2xl font-semibold mb-2'>Postnatal Care</h3>
+              <p className='text-gray-700 mb-4'>
+                We give postnatal care (PNC) to the mother and her newborn baby
+                after first six weeks of life. This is important because
+                effective PNC helps in recognizing any deviation from expected
+                recovery after birth the health of the baby allowing prompt
+                intervention if needed.
+              </p>
+            </li>
+            <li>
+              <h3 className='text-2xl font-semibold mb-2'>Well-baby Clinic</h3>
+              <p className='text-gray-700 mb-4'>
+                FemiHub Well-Baby Services are designed to monitor your baby's
+                growth through a series of regular wellness check-ups for
+                newborn babies and infants. Services are provided by a team of
+                experienced nurses and doctors the services we offer in this
+                clinic include:
+                <ul className='list-disc p-8'>
+                  <li>
+                    Monitoring of your baby's growth and developmental progress
+                    at regular intervals
+                  </li>
+                  <li>Vaccinations</li>
+                  <li>Examinations for common diseases</li>
+                  <li>Advice on nutrition</li>
+                  <li>
+                    Information on preventing childhood illness and your baby's
+                    growth
+                  </li>
+                </ul>
+              </p>
+            </li>
+            <li>
+              <h3 className='text-2xl font-semibold mb-2'>
+                Family Planning and Child Spacing
+              </h3>
+              <p className='text-gray-700 mb-4'>
+                We provide family planning and contraceptive services to enable
+                couples to decide when and if to have children, using the
+                information, means and methods they need to do so.
+              </p>
+            </li>
+          </ul>
+          <h3 className='text-3xl font-semibold mt-12 mb-8'>
+            Why Choose FemiHub for Maternal Wellness
+          </h3>
+          <ul className='px-6 md:px-16 list-disc'>
+            <li>
+              <p className='text-gray-700 mb-4'>
+                <strong>Expert and Compassionate Care:</strong> FemiHub boasts a
+                team of skilled healthcare professionals specializing in
+                maternal and child health, including nurses and doctors. They
+                have extensive knowledge and experience in caring for expectant
+                mothers, newborns, and young children.
+              </p>
+            </li>
+            <li>
+              <p className='text-gray-700 mb-4'>
+                <strong>Continuity of Care:</strong> Our commitment to providing
+                comprehensive care begins when you discover you're pregnant and
+                continues throughout the early years of your child's life. We
+                offer antenatal care to monitor and support you during
+                pregnancy, postnatal care to ensure a healthy recovery for both
+                mother and baby, and well-baby clinic services to track your
+                child's growth and development.
+              </p>
+            </li>
+            <li>
+              <p className='text-gray-700 mb-4'>
+                <strong> Emphasis on Preventive Care:</strong> We believe in the
+                power of prevention. We aim to prevent illness and promote
+                healthy development for mothers and children through regular
+                check-ups, vaccinations, and screenings. Our well-baby clinic
+                monitors growth and milestones and provides valuable information
+                on preventing childhood illnesses.
+              </p>
+            </li>
+            <li>
+              <p className='text-gray-700 mb-4'>
+                <strong>Personalized Support:</strong>Every mother and child is
+                unique, and we recognize the importance of personalized care.
+                Whether you need advice on nutrition, assistance with
+                breastfeeding, or information on family planning, our healthcare
+                professionals are dedicated to helping you make informed
+                decisions that are best for you and your family.
+              </p>
+            </li>
+          </ul>
+          <p className='text-gray-700 mb-4 text-center'></p>
+          <div className='w-full flex justify-center'>
+            <a
+              href='https://wa.me/256743799700'
+              className='my-4 inline-block bg-custom-pink text-white px-6 py-3 rounded-full shadow-md hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 mx-auto'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Book an Online Consultation
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MaternalWellness;
--- client/src/components/PEPInfo.jsx
+++ client/src/components/PEPInfo.jsx
@@ -1,108 +1,130 @@
 import React from "react";
 import pepPills from "../assets/pep-pills.jpeg";
+import HIVTesting from "../assets/hiv-testing.png";
 
 const PEPInfo = () => {
   return (
-    <section className='bg-white min-h-screen flex items-center justify-center py-16 px-4 md:px-8'>
+    <section className='bg-white min-h-screen flex items-center justify-center pb-16 px-4 md:px-8'>
       <div className='mx-auto flex flex-col items-center'>
-        <div className='md:w-4/5 px-8 mb-8'>
+        <h2 className='text-2xl md:text-4xl font-bold text-custom-pink my-8 text-center'>
+          Post-Exposure Prophylaxis (PEP)
+        </h2>
+        <div className='w-full md:w-4/5 md:px-8 mb-8'>
           <img
             src='/images/pep.png'
             alt='PrEP Information'
-            className='w-full h-96 rounded-lg shadow-md'
+            className='w-full md:h-96'
           />
         </div>
-        <div className='md:w-4/5 text-gray-600 bg-white'>
-          <p className='px-8'>
+        <div className='w-full md:w-4/5 text-gray-600 bg-white'>
+          <p className='md:px-8'>
             Post-exposure prophylaxis (PEP) is used by HIV negative people after
             a possible exposure to HIV and should be started within 72 hours of
             exposure. PEP is for emergency situations. If taken 72 hours after
             possible exposure, PEP is highly effective in preventing HIV. PEP is
             safe but may cause side effects like nausea in some people.
           </p>
-          <p className='px-8 py-2'>
+          <p className='md:px-8 py-2'>
             If you find yourself taking PEP regularly or believe yourself to be
             regularly exposed to HIV then you should start taking PrEP instead.
             PrEP is taken before exposure by HIV negative people who are at risk
             of acquiring HIV. PrEP reduces your chances of being infected with
             HIV.
           </p>
-          <h2 className='text-2xl font-bold mt-4 px-8'>Facts about PEP</h2>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-            <div className='p-6 rounded-lg'>
+          <h2 className='text-2xl font-bold mt-4 px-4 md:px-8'>
+            Facts about PEP
+          </h2>
+          <ul className='list-disc px-8 md:px-12'>
+            <li className='py-2'>
               <p className='text-gray-700'>
                 PEP are ARV's that may be used to prevent HIV infection in a
                 person exposed to HIV within 72 hours. You must take PEP within
                 72 hours of exposure for it to work optimally.
               </p>
-            </div>
-            <div className='p-6 rounded-lg'>
+            </li>
+            <li className='py-2'>
               <p className='text-gray-700'>
                 PEP will most likely not prevent HIV infection if it is started
                 more than 72 hours after exposure.
               </p>
-            </div>
-            <div className='p-6 rounded-lg'>
+            </li>
+            <li className='py-2'>
               <p className='text-gray-700'>
                 You must take one pill daily for 28 days without skipping a
                 dose.
               </p>
-            </div>
-            <div className='p-6 rounded-lg'>
+            </li>
+            <li className='py-2'>
               <p className='text-gray-700'>
                 A HIV test is done prior to initiating PEP, 28 days after taking
                 the first PEP dose and 3 months after the initial HIV exposure.
                 PEP will not protect you from unplanned pregnancy of other STIs.
               </p>
-            </div>
-          </div>
-          <div className='md:w-4/5 text-gray-600 px-8'>
-            <h2 className='text-2xl font-bold my-4'>
-              Quick guide to starting PEP
-            </h2>
-            <p>
-              For PEP to work, it must be taken within 72 hours of exposure. The
-              sooner you start it the better, every minute counts. You must test
-              HIV negative to start PEP. You need to take PEP daily dose for 28
-              days. It is given in a pill format. Missing doses of PEP can make
-              the treatment less effective. It may be helpful to set a daily
-              reminder to remember the time for your dose.
-              <img src={pepPills} alt='PEP Pills' className='h-32 mt-3 mx-auto' />
-            </p>
-            <h2 className='text-2xl font-bold my-4'>
-              What is a PEP Assessment Like
-            </h2>
-            <p>
-              Exposure to HIV Is an emergency because HIV establishes infection
-              very quickly. A health care worker will evaluate you rapidly for
-              PEP initiation when care is sought within 72 hours after potential
-              exposure. A HIV test will be done and once confirmed negative PEP
-              will be prescribed.
-            </p>
-            <h2 className='text-2xl font-bold my-4'>HIV Self-testing</h2>
-            <p>
-              If you have been exposed to HIV infection for instance through a
-              burst or broken condom, unprotected sex from a person with a
-              positive or unknown HIV status, you will need to test negative for
-              HIV to be eligible for PEP. It is important to check that you did
-              not have infection before this risk. There are lots of HIV
-              Self-Tests (HIVSTs) in the market. MyFemiHub offers two tests for
-              clients onboard onto PrEP - Mylan and Sure Check. Only Mylan can
-              be used onboard through MyFemiHub because of the uniqueness of the
-              online onboarding journey.
-            </p>
-            <h2 className='text-2xl font-bold my-4'>PrEP after PEP</h2>
-            <p>
-              If you are continously engaging in behaviour that exposes you to
-              contracting HIV or you frequently need PEP, you should consider
-              PrEP to protect yourself. After you have finished your 28 day
-              course of PEP, and test again for HIV, if you are negative you
-              should book a consultation to talk with one of MyFemiHub's
-              Clinical Officers about PrEP to protect yourself long-term from
-              HIV infection. Taking PrEP will enable you to continue to live
-              your life as you are while keeping yourself and your relationships
-              HIV free.
-            </p>
+            </li>
+          </ul>
+          <h2 className='text-2xl font-bold my-4'>
+            Quick guide to starting PEP
+          </h2>
+          <p>
+            For PEP to work, it must be taken within 72 hours of exposure. The
+            sooner you start it the better, every minute counts. You must test
+            HIV negative to start PEP. You need to take PEP daily dose for 28
+            days. It is given in a pill format. Missing doses of PEP can make
+            the treatment less effective. It may be helpful to set a daily
+            reminder to remember the time for your dose.
+            <img
+              src={pepPills}
+              alt='PEP Pills'
+              className='w-full md:w-auto h-32 mt-3 mx-auto'
+            />
+          </p>
+          <h2 className='text-2xl font-bold my-4'>
+            What is a PEP Assessment Like
+          </h2>
+          <p>
+            Exposure to HIV Is an emergency because HIV establishes infection
+            very quickly. A health care worker will evaluate you rapidly for PEP
+            initiation when care is sought within 72 hours after potential
+            exposure. A HIV test will be done and once confirmed negative PEP
+            will be prescribed.
+          </p>
+          <h2 className='text-2xl font-bold my-4'>HIV Self-testing</h2>
+          <img
+            src={HIVTesting}
+            alt='HIV Testing'
+            className='w-full md:w-auto md:h-32 my-4 mx-auto'
+          />
+          <p>
+            If you have been exposed to HIV infection for instance through a
+            burst or broken condom, unprotected sex from a person with a
+            positive or unknown HIV status, you will need to test negative for
+            HIV to be eligible for PEP. It is important to check that you did
+            not have infection before this risk. There are lots of HIV
+            Self-Tests (HIVSTs) in the market. MyFemiHub offers two tests for
+            clients onboard onto PrEP - Mylan and Sure Check. Only Mylan can be
+            used onboard through MyFemiHub because of the uniqueness of the
+            online onboarding journey.
+          </p>
+          <h2 className='text-2xl font-bold my-4'>PrEP after PEP</h2>
+          <p>
+            If you are continously engaging in behaviour that exposes you to
+            contracting HIV or you frequently need PEP, you should consider PrEP
+            to protect yourself. After you have finished your 28 day course of
+            PEP, and test again for HIV, if you are negative you should book a
+            consultation to talk with one of MyFemiHub's Clinical Officers about
+            PrEP to protect yourself long-term from HIV infection. Taking PrEP
+            will enable you to continue to live your life as you are while
+            keeping yourself and your relationships HIV free.
+          </p>
+          <div className='w-full flex justify-center'>
+            <a
+              href='https://wa.me/256743799700'
+              className='my-4 inline-block bg-custom-pink text-white px-6 py-3 rounded-full shadow-md hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 mx-auto'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Book an Online Consultation
+            </a>
           </div>
         </div>
       </div>
